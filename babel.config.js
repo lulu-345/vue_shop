59cc36a5c89a88b2c8f0a8d3507a114697e7aa5d
lushun <1649209299@qq.com>
2020-11-03 13:32:43 +0800

    添加了生产模式下面的去除console的功能

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// 定义一个用于存放生产模式下面的插件的数组
+const prodPlugins = []
+// 进行判断如果是生产模式就把插件push进去
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
@@ -9,6 +16,7 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    ...prodPlugins
   ]
 }
